5a04be79d64cba8937a83531e505f70c37dc3ed2
KishoreKumar0603 <kishorekumars5643@gmail.com>
2025-04-30 13:44:34 +0530

    all covered

--- frontend/src/Components/Wishlist/RootWishlist.jsx
+++ frontend/src/Components/Wishlist/RootWishlist.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Wishlist } from '../profileComponents/Wishlist'
+import { Wishlist } from '../profileComponents/Wishlist';
+import { useEffect } from 'react';
 export const RootWishlist = () => {
   useEffect(()=>{
         document.title=`BuyIt | Wishlists`;
